a04d585c71db9ce695b57b2a15048e3747433400
Oliver Fleckenstein <oliverfl@live.dk>
2022-10-22 11:11:03 +0100

    feat: Listening for terminal signals

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,13 +1,44 @@
-use std::io;
-use tokio::spawn;
-use websockets::{Frame, WebSocket};
+use futures::stream::StreamExt;
+use signal_hook::consts::{SIGINT, SIGQUIT, SIGTERM};
+use signal_hook_tokio::Signals;
+use std::{
+    io::{self, Error},
+    sync::Arc,
+};
+use tokio::{spawn, sync::Mutex};
+use websockets::{Frame, WebSocket, WebSocketReadHalf, WebSocketWriteHalf};
 
 #[tokio::main]
-async fn main() {
+async fn main() -> Result<(), Error> {
     let host = "ws://localhost:3030/stream";
+    let (rx, tx) = WebSocket::connect(host).await.unwrap().split();
+    let tx = Arc::new(Mutex::new(tx));
 
-    let (mut rx, mut tx) = WebSocket::connect(host).await.unwrap().split();
+    let signals = Signals::new(&[SIGINT, SIGTERM, SIGQUIT])?;
+    let handle = signals.handle();
+    let signals_task = spawn(handle_signals(signals, tx.clone()));
 
+    run_chat(rx, tx).await;
+
+    _ = handle.clone();
+    _ = signals_task;
+
+    Ok(())
+}
+
+async fn handle_signals(mut signals: Signals, tx: Arc<Mutex<WebSocketWriteHalf>>) {
+    while let Some(signal) = signals.next().await {
+        match signal {
+            SIGTERM | SIGINT | SIGQUIT => {
+                disconnect(&tx).await;
+                return;
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+async fn run_chat(mut rx: WebSocketReadHalf, tx: Arc<Mutex<WebSocketWriteHalf>>) {
     let mut user_input = String::new();
     let stdin = io::stdin();
 
@@ -24,26 +55,36 @@ async fn main() {
         if user_input.starts_with('/') {
             match user_input.trim_end() {
                 "/exit" => {
-                    match tx.close(None).await {
-                        Ok(()) => {
-                            println!("Disconnected");
-                            break;
-                        }
-                        Err(e) => eprintln!("Unable to disconnect: {e}"),
-                    };
+                    disconnect(&tx).await;
+                    break;
                 }
                 _ => eprintln!("Unknown command"),
             };
         } else {
             // Send message to server
+            let mut tx = tx.lock().await;
             match tx
                 .send_text(user_input.clone().trim_end().to_string())
                 .await
             {
                 Ok(()) => {}
-                Err(e) => eprintln!("Failed to send message: {e}"),
+                Err(e) => {
+                    eprintln!("Failed to send message: {e}");
+                    break;
+                }
             };
         }
         user_input.clear();
     }
+}
+
+async fn disconnect(tx: &Arc<Mutex<WebSocketWriteHalf>>) {
+    let mut tx = tx.lock().await;
+    match tx.close(None).await {
+        Ok(()) => {
+            println!("Disconnected");
+            return;
+        }
+        Err(e) => eprintln!("Unable to disconnect: {e}"),
+    };
 }
