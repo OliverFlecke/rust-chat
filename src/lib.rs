ff78c7458a488dea65a35fb4a4c06f9dd28617f6
Oliver Fleckenstein <oliverfl@live.dk>
2022-10-24 11:06:16 +0100

    feat: Simple encryption of `ChatMessage`

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 use std::fmt::Display;
 
 use derive_getters::Getters;
+use orion::{aead, kex::SecretKey};
 use serde::{Deserialize, Serialize};
 
 type UserId = String;
 
-#[derive(Debug, Getters, Serialize, Deserialize)]
+#[derive(Debug, Getters, Serialize, Deserialize, PartialEq, Eq)]
 pub struct ChatMessage {
     sender: UserId,
     text: String,
@@ -33,6 +34,14 @@ impl ChatMessage {
     pub fn serialize(&self) -> String {
         serde_json::to_string(self).unwrap()
     }
+
+    pub fn encrypt(&self, secret_key: &SecretKey) -> Vec<u8> {
+        aead::seal(secret_key, self.serialize().as_bytes()).unwrap()
+    }
+
+    pub fn decrypt(secret_key: &SecretKey, cipher_text: &[u8]) -> Self {
+        serde_json::from_slice(aead::open(secret_key, cipher_text).unwrap().as_slice()).unwrap()
+    }
 }
 
 impl Display for ChatMessage {
@@ -43,6 +52,8 @@ impl Display for ChatMessage {
 
 #[cfg(test)]
 mod test {
+    use orion::aead;
+
     use super::*;
 
     #[test]
@@ -51,5 +62,20 @@ mod test {
 
         assert_eq!(msg.sender(), "username");
         assert_eq!(msg.text(), "Hello there!");
+    }
+
+    #[test]
+    fn encryption() {
+        let secret_key = aead::SecretKey::default();
+        let msg = ChatMessage::new("sender".to_string(), "message".to_string());
+
+        let encrypted_msg = msg.encrypt(&secret_key);
+
+        assert!(!encrypted_msg.is_empty());
+
+        // Decrypt
+        let decrypted = ChatMessage::decrypt(&secret_key, &encrypted_msg);
+
+        assert_eq!(msg, decrypted);
     }
 }
