49e47e4a557e69d0034a5d18760083e2ae0b9bd0
Oliver Fleckenstein <oliverfl@live.dk>
2022-11-08 17:54:08 +0000

    build: Updated `run_server.sh` script to ignore changes made to the client when running in hot-reload mode

--- run_server.sh
+++ run_server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-systemfd --no-pid -s http::3030 -- cargo watch -x 'run --bin chat-server'
+systemfd --no-pid -s http::3030 -- cargo watch --ignore chat-client -x 'run --bin chat-server'
